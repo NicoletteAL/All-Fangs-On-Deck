91f1dea13169efff84fdc83d02025a168a302266
MEMaicoSt <mary.steele@my.utsa.edu>
2023-09-17 23:11:02 -0500

    Trying to commit some stuff

--- Assets/PlayerInputController.cs
+++ Assets/PlayerInputController.cs
@@ -6,6 +6,9 @@ public class PlayerInputController : MonoBehaviour
 {
 
     creature creature;
+
+    public Character_animation_switch sw;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -39,13 +42,27 @@ public class PlayerInputController : MonoBehaviour
         if(Input.GetKey(KeyCode.D))
         {
             creature.Move(new Vector3(1,0,0));
+            creature.transform.localScale = new Vector3(2, 2, 1);
+            sw.next_Animation_For_Prince("Prince_Run"); 
+            
+
         } else if(Input.GetKey(KeyCode.A))
         {
             creature.Move(new Vector3(-1,0,0));
-        } 
+            creature.transform.localScale = new Vector3(-2, 2, 1);
+            sw.next_Animation_For_Prince("Prince_Run");
+
+        }
+
+        else
+        {
+            sw.next_Animation_For_Prince("Standing_There");
+        }
         //if(Input.GetKey(KeyCode.Space))
         //{
             //creature.Jump();
         //}
     }
 }
+
+//reference for sprite flip: https://www.youtube.com/watch?v=Cr-j7EoM8bg
