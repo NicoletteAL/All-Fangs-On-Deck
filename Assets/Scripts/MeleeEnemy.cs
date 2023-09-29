b2788a11e93e92b0101e28ea6e1b71c700191e3e
Elisha Lucile O <lucile.ong@my.utsa.edu>
2023-09-28 19:25:33 -0500

    Updated Health.cs
    Separated health functions into its own script, updated meleeenemy prefab, updated creature script

--- Assets/Scripts/MeleeEnemy.cs
+++ Assets/Scripts/MeleeEnemy.cs
@@ -14,14 +14,7 @@ public class MeleeEnemy : MonoBehaviour
 
     private float cooldownTimer = Mathf.Infinity;
 
-    //temp
     public Health playerHealth;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -40,15 +33,22 @@ public class MeleeEnemy : MonoBehaviour
 
     public bool PlayerInSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
-        new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z)
-        , 0, Vector2.left, 0, playerLayer);
+        RaycastHit2D hit = PerformBoxCast();
 
         if (hit.collider != null)
         {
             playerHealth = hit.transform.GetComponent<Health>();
         }
+
         return hit.collider != null;
+    }
+
+    private RaycastHit2D PerformBoxCast()
+    {
+        Vector2 castOrigin = boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance;
+        Vector3 castSize = new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z);
+
+        return Physics2D.BoxCast(castOrigin, castSize, 0, Vector2.left, 0, playerLayer);
     }
 
     private void OnDrawGizmos()
--- Assets/Scripts/creature.cs
+++ Assets/Scripts/creature.cs
@@ -6,11 +6,10 @@ public class creature : MonoBehaviour
 {
     [Header("Config")]
     public int health;
-    public int maxHealth = 3;  //TODO: change to 100 and update HealthDisplay .. % 10
+    public int maxHealth = 3;  
     public float speed = 6.0f;
     public float jumpForce = 35f;
     public string creatureName = "Lonk";
-    private float DMG_DELAY = 1.5f;
 
     [Header("Projectiles")]
     public GameObject projectile;
@@ -30,7 +29,6 @@ public class creature : MonoBehaviour
     void Start()
     {
         Debug.Log("start called");
-        health = maxHealth;
     }
 
     // Update is called once per frame
@@ -66,53 +64,4 @@ public class creature : MonoBehaviour
         //newProjectile.GetComponent<projectile>().LaunchProjectile(Camera.main.ScreenToWorldPoint(Input.mousePosition));
     }
 
-    /*Health Functions*/
-    public void TakeDamage(int d) 
-    {
-        Debug.Log("took damage");
-        if (health <= 0)
-        {
-            Debug.Log("You died");
-            //TODO: You Died text overlay
-        } else {
-            health -= d;
-        }
-    }
-
-    public void GainHealth(int h)
-    {
-        //play sound effect
-        health += h;
-    }
-
-    /*Collisions / Triggers*/
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            TakeDamage(1); //temp
-            StartCoroutine(DamageDelay());
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        GameObject collidedObject = collider.gameObject;
-
-        if (collidedObject.tag != "Item")
-            return;
-
-        //switch statement if there are more items
-
-        if (health != maxHealth)
-        {
-            GainHealth(1); //temp value
-            Destroy(collider.gameObject);
-        }
-    }
-
-    IEnumerator DamageDelay()
-    {
-        yield return new WaitForSeconds(DMG_DELAY);
-    }
 }
