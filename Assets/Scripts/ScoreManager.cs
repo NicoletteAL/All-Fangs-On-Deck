292655601ce87afac238f44864ca60a67ddc38c8
Lulu <ongelishalu@gmail.com>
2023-12-02 19:59:38 -0800

    Score Label fix

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public Text scoreLabel;
     public int currentScore;
+
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Level 1" || scene.name == "Level 2")
+        {
+            scoreLabel = GameObject.Find("ScoreLabel").GetComponent<Text>();
+            updateScore(0);
+        } else 
+        {
+            currentScore = 0;
+            scoreLabel.text = "";
+        }
+    }
 
     public void updateScore(int score) 
     {
